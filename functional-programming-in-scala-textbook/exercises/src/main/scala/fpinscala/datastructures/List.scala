1b45e593f35fd368e0ab8faf6e033fabf709aed5
Udesh Senaratne <udesh@streetcontxt.com>
2015-03-11 12:35:52 -0400

    FP in Scala: Completed upto exercise 3.12

--- functional-programming-in-scala-textbook/exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ functional-programming-in-scala-textbook/exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 case class Cons[+A](head: A, tail: List[A]) extends List[A] // Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`, which may be `Nil` or another `Cons`.
@@ -47,19 +49,106 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  //exercise 3.2
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => throw new RuntimeException("Attempted to obtain tail of empty list")
+      case Cons(_, tail) => tail
+    }
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  //exercise 3.3
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Nil => List(h) //the answer throws an exception. How would you express an empty list, and set head ? s
+      case Cons(oldHead, tail) => Cons(h, tail)
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  //exercise 3.4 (the answer in this case doesn't throw exception when l.size<n)
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0){
+      l
+    } else {
+	    l match {
+	      case Nil => throw new RuntimeException("Cannot drop " + n + " elements in a smaller list")
+	      case Cons(_, tail) => drop(tail, n-1)
+	    }      
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  //exercise 3.5 
+  /*answer section solution seems weird
+   consider: dropWhile(List(3, 4, 4, 5, 6, 1, 7), (n:Int)=>{ n % 2 == 1}) 
+   project answers
+   scala> dropWhile(List(3, 4, 4, 5, 6, 1, 7), (n:Int)=>{ n % 2 == 1})
+res0: fpinscala.datastructures.List[Int] = Cons(4,Cons(4,Cons(5,Cons(6,Cons(1,Cons(7,Nil))))))
+   project exercises
+   scala> dropWhile(List(3, 4, 4, 5, 6, 1, 7), (n:Int)=>{ n % 2 == 1})
+res2: fpinscala.datastructures.List[Int] = Cons(4,Cons(4,Cons(6,Nil)))
+   i.e my answer is (4, 4, 6) which is correct, given answer is (4, 4, 5, 6, 1, 7)  
+   
+   ^^ the answer is correct, misunderstood dropwhile
+   */
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(head, tail) => if (f(head)) dropWhile(tail, f) else Cons(head,dropWhile(tail, f))
+      case Nil => l
+    }
+  }
+  
+  //exercise 3.6
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => throw new RuntimeException("Attempted to get initial elements of a null list")
+      case Cons(_, Nil) => l
+      case Cons(head, tail) => Cons(head, init(tail))
+    }
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  //exercise 3.9
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((h,tailAcc) => 1 + tailAcc)
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
-
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  //exercise 3.10
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    l match {
+      case Nil=> z
+      case Cons(h, tail) => foldLeft(tail,f(z, h))(f)
+    } 
+  }
+  
+  //exercise 3.11a
+  def sumFL(xs: List[Int]): Int = {
+    foldLeft(xs, 0)(_+_)
+  }
+  
+  //exercise 3.11b
+  def productFL(xs: List[Int]): Int = {
+    foldLeft(xs, 1)(_*_)
+  }  
+  
+  //exercise 3.11c
+  def lengthFL[A](xs: List[A]): Int = {
+    foldLeft(xs, 1)((acc,_)=>(acc+1))
+  }
+  
+  def reverse[A](xs: List[A]): List[A] = {
+    foldLeft(xs, List[A]())((tail,h)=>Cons(h, tail))
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
+
+
+//Exercise 3.1
+//1 + 2 = 3
+
+//Exercise 3.7
+//Unless foldright is modified, there is no way to short circut * 0
+
+//Exercise 3.8
+//what happens when you pass Nil and Cons themselves to foldRight?
+//You'll essentially get a copy.  Fold right and data constructors of List walk the tree the same way
