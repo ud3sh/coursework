3e2317016075961abeb14d1491f0c3203b2ec005
Udesh Senaratne <udesh@streetcontxt.com>
2015-12-26 00:08:54 -0500

    Week two assignment

--- coursera-machine-learning-2015-fall/ex2/costFunction.m
+++ coursera-machine-learning-2015-fall/ex2/costFunction.m
@@ -7,8 +7,10 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
+
 % You need to return the following variables correctly 
-J = 0;
+n = size(X)(2);
+J = 1/m  * sum( -y' * log(sigmoid(X * theta)) - (1-y)' * log(1 - sigmoid(X * theta))); 
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -19,13 +21,6 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
-
-
-
-
-
 
 % =============================================================
 
--- coursera-machine-learning-2015-fall/ex2/costFunctionReg.m
+++ coursera-machine-learning-2015-fall/ex2/costFunctionReg.m
@@ -8,7 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+n = size(X)(2);
+J = 1/m  * sum( -y' * log(sigmoid(X * theta)) - (1-y)' * log(1 - sigmoid(X * theta))) + (lambda/(2 * m)) * sum(theta(2 : n) .^ 2);
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
