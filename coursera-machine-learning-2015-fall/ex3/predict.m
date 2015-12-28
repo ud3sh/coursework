69943ac85a79e3b9c21616a594c2b48bdb565259
Udesh Senaratne <udesh@streetcontxt.com>
2015-12-28 01:48:47 -0500

    Implemented Week 4 assignment on Multi-class Classication and Neural Networks

--- coursera-machine-learning-2015-fall/ex3/predict.m
+++ coursera-machine-learning-2015-fall/ex3/predict.m
@@ -7,29 +7,17 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+% ====================== YOUR CODE HERE =======================
+
+a1 = [ones(m,1) X];
+a2 = [ones(m, 1) sigmoid(a1 * Theta1')];
+a3 = sigmoid(a2 * Theta2');
+[max index] = max(a3, [], 2);
+
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = index;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
+% =============================================================
 
 
 end
--- coursera-machine-learning-2015-fall/ex3/predictOneVsAll.m
+++ coursera-machine-learning-2015-fall/ex3/predictOneVsAll.m
@@ -11,32 +11,16 @@ function p = predictOneVsAll(all_theta, X)
 m = size(X, 1);
 num_labels = size(all_theta, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% ====================== YOUR CODE HERE =======================
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
+classification = sigmoid(X * all_theta');
+[max index] = max(classification, [], 2);
 
+% You need to return the following variables correctly
+p = index;
 
-
-
-
-
-
-% =========================================================================
-
-
+% =============================================================
 end
