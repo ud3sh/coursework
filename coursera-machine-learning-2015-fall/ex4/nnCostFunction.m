44b4a523c02e5c4e6665b22ab6e44461a8010e77
Udesh Senaratne <udesh@streetcontxt.com>
2016-01-01 10:36:57 -0500

    Completed Week 5 assignment on Neural Networks

--- coursera-machine-learning-2015-fall/ex4/nnCostFunction.m
+++ coursera-machine-learning-2015-fall/ex4/nnCostFunction.m
@@ -25,11 +25,6 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 % Setup some useful variables
 m = size(X, 1);
          
-% You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
 %               following parts.
@@ -62,30 +57,46 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+n = size(X, 2);
+k = num_labels;
+costs = zeros(m, 1);            %initialize the cost for each entry in the training set to be 0
+Theta1_grad = zeros(size(Theta1));
+Theta2_grad = zeros(size(Theta2));
 
+for i = 1:m 
+    A1_i = [1 X(i,:)]';         % A1_i is simply the ith entry with a bias value as the first item in the vector 
+    Z2_i = Theta1 * A1_i;    
 
+    A2_i = [1; sigmoid(Z2_i)];
+    Z3_i = Theta2 * A2_i;
+    
+    A3_i = sigmoid(Z3_i);
 
+    y_i = zeros(k, 1);           %vectorizing the result for the ith entry
+    y_i(y(i)) = 1;
 
+    delta3_i = A3_i - y_i;
+    delta2_i = Theta2(:, 2:end)' * delta3_i .* sigmoidGradient(Z2_i);
+    
+    Theta2_grad = Theta2_grad + delta3_i * A2_i';
+    Theta1_grad = Theta1_grad + delta2_i * A1_i';
 
+    costs(i) =  -y_i' * log(A3_i) - (1-y_i)' * log(1 - A3_i);
+end
 
+theta1RegSum = sumsq( Theta1(:, 2 : end)(:) );   % end = input_layer_size + 1 = size(Theta1, 2);
+theta2RegSum = sumsq( Theta2(:, 2 : end)(:));    % end = hidden_layer_size + 1 = size(Theta2, 2);
 
+regularizationPartialSum = (lambda/(2 * m)) * (theta1RegSum + theta2RegSum);
 
+J = 1/m * sum(costs) + regularizationPartialSum;
 
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [ zeros( size(Theta1, 1), 1) Theta1(:,2:end) ];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [ zeros( size(Theta2, 1), 1) Theta2(:,2:end) ];
 
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
+% ============================================================
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
